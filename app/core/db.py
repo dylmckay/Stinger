66cbfbe36f397fbce444e336cf700a7432dcdcea
Dylan <dylanmckay2004@gmail.com>
2026-06-16 13:24:55 -0400

    Remove unsued db.py file

--- app/core/db.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.config import get_settings
-
-engine = create_engine(get_settings().DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
